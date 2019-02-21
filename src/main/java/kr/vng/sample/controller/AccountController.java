1ab1f81fbfc08a01ee58dcbbf5a31d388a83ea99
KJE <KJE@DESKTOP-4SMA80D>
2019-02-21 17:53:04 +0900

    로그인 기능 추가 2차

---
+++ src/main/java/kr/vng/sample/controller/AccountController.java
@@ -0,0 +1,18 @@
+package kr.vng.sample.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class AccountController {
+	
+	
+	
+	@RequestMapping(value = "/register.do", method = {RequestMethod.GET, RequestMethod.POST})
+	public String register() {
+		
+		return "";
+		
+	}
+}
--- src/main/webapp/WEB-INF/view/loginPage.jsp
+++ src/main/webapp/WEB-INF/view/loginPage.jsp
@@ -1,6 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 <%@ taglib prefix="include" tagdir="/WEB-INF/tags/include" %>
-<%@ taglib prefix="login" tagdir="/WEB-INF/tags/account" %>
+<%@ taglib prefix="login" tagdir="/WEB-INF/tags/login" %>
 
 <html>
 <head>
